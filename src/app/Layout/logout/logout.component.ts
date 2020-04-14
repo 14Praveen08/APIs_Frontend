ce1e72978de60d3ba8948e87bfa424040cf49f76
Praveen Kumaar <actionking148@gmail.com>
2020-04-14 20:24:15 +0530

    Changes in displaying empty records

--- src/app/Layout/logout/logout.component.ts
+++ src/app/Layout/logout/logout.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '../../Authentication/authentication.service';
 import { Router } from '@angular/router';
+import { ToastrService } from 'ngx-toastr';
 
 @Component({
   selector: 'app-logout',
@@ -9,10 +10,15 @@ import { Router } from '@angular/router';
 })
 export class LogoutComponent implements OnInit {
 
-  constructor(private _auth: AuthenticationService, private router: Router) { }
+  constructor(private toaster: ToastrService,private _auth: AuthenticationService, private router: Router) { }
 
   ngOnInit(): void {
     this._auth.logOut();
+    this.toaster.success('Logging Out','Success',{
+      progressBar:true,
+      timeOut:1000,
+      progressAnimation:"decreasing"
+    });
     this.router.navigate(['']);
   }
 
